ae9c3f8096145c7317acf97257877bfc3d0ca2d1
yingzy <546188155@qq.com>
2020-05-11 03:31:48 -0700

    sql语句报错

--- src/init_sql/v1.7.0-my.sql
+++ src/init_sql/v1.7.0-my.sql
@@ -7,9 +7,9 @@ INSERT INTO auth_permission
 (name, content_type_id, codename)
 VALUES('查看所有上线sql', 24, 'sql_read_only');
 
-ALTER TABLE sql_user
-    ADD employee varchar(50) CHARACTER NULL COMMENT '员工类型',
-    ADD manager varchar(126) CHARACTER NULL COMMENT '领导';
+ALTER TABLE sql_users
+    ADD employee varchar(50) NULL COMMENT '员工类型',
+    ADD manager varchar(126) NULL COMMENT '领导';
 
 ALTER TABLE workflow_audit
-    ADD pre_audit varchar(50) CHARACTER NULL COMMENT '前置审批者';
+    ADD pre_audit varchar(50) NULL COMMENT '前置审批者';
